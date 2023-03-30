09757726a0098b543d5b374334be78eb56e0e3d3
dhirtiman <dhirtimankhati@gmail.com>
2023-03-30 20:07:39 +0530

    eh

--- AddTwo.java
+++ AddTwo.java
@@ -4,11 +4,16 @@ public class AddTwo {
 
     public static void main(String[] args) {
         int num1,num2,sum;
+
         Scanner scanner  = new Scanner(System.in);
+
         System.out.println("Enter two numbers:");
+
         num2 = scanner.nextInt();
         num1 = scanner.nextInt();
+
         sum = num1 + num2 ;
+        
         System.out.println("sum: "+sum);
 
         scanner.close();
