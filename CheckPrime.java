91dcb05ac27f4fd607e628a9be1f583b0c178b98
dhirtiman <dhirtimankhati@gmail.com>
2023-03-30 14:16:26 +0530

    Optimize

--- CheckPrime.java
+++ CheckPrime.java
@@ -10,7 +10,7 @@ public class CheckPrime {
         n = sc.nextInt();
         if (n != 1 && n != 0) {
 
-            for (int i = 2; i < n; i++) {
+            for (int i = 2; i*i <= n; i++) {
                 if (n % i == 0) {
                     prime = 0;
                     break;
