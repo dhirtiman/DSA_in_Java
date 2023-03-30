6b9d126fabffdf42c5c55d3c00993084c2223152
dhirtiman <dhirtimankhati@gmail.com>
2023-03-30 13:01:50 +0530

    moreDSA

---
+++ Salary.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Salary {
+    public static void main(String[] args) {
+        Double salary;
+        Scanner sc  = new Scanner(System.in);
+        salary = sc.nextDouble();
+        
+        System.out.println("Salary before bonus: "+String.format("%.0f", salary));
+
+        if (salary > 10000) 
+        {
+            salary+=2000;    
+        } else {
+            salary+=1000;
+        }
+
+        System.out.print("Salary after Bonus: ");
+        System.out.println(String.format("%.0f", salary));
+    }
+    
+}
